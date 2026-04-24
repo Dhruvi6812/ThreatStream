06a4872f4592defc6abae13c186ef239ad545208
Dhruvi6812 <dhruvi211001@gmail.com>
2026-04-24 15:09:01 -0400

    fix CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,20 +1,21 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.threats import router as threat_router
 from app.api.ip_routes import router as ip_router
 
 app = FastAPI(title="ThreatStream API")
 
-@app.middleware("http")
-async def cors_fix(request, call_next):
-    print("MIDDLEWARE RUNNING")   # ADD THIS
-    response = await call_next(request)
-
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "*"
-    response.headers["Access-Control-Allow-Headers"] = "*"
-
-    return response
+origins = [
+    "https://threat-stream-five.vercel.app",
+    "http://localhost:3000"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(threat_router)
 app.include_router(ip_router)
